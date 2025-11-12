5078411ac36c118fa1bb15379acd0bc8c142a5b7
sunsunarto <sunaryo24259@outlook.com>
2025-11-12 07:52:28 +0700

    2  commit

---
+++ data.js
@@ -0,0 +1,64 @@
+const students = [
+        {
+            "id": 1,
+            "name": "Alice Johnson",
+            "age": 21,
+            "grade": "90",
+        },
+        {
+            "id": 2,
+            "name": "Bob Smith",
+            "age": 20,
+            "grade": "70",
+        },
+        {
+            "id": 3,
+            "name": "Charlie Brown",
+            "age": 22,
+            "grade": "100",
+        },
+        {
+            "id": 4,
+            "name": "Diana Prince",
+            "age": 19,
+            "grade": "60",
+        },
+        {
+            "id": 5,
+            "name": "Ethan Hunt",
+            "age": 21,
+            "grade": "80",
+        },
+        {
+            "id": 6,
+            "name": "Fiona Glenanne",
+            "age": 20,
+            "grade": "95",
+        },
+        {
+            "id": 7,
+            "name": "George Lucas",
+            "age": 23,
+            "grade": "76",
+        },
+        {
+            "id": 8,
+            "name": "Hannah Montana",
+            "age": 19,
+            "grade": "80",
+        },
+        {
+            "id": 9,
+            "name": "Ivy Valentine",
+            "age": 22,
+            "grade": "50",
+        },
+        {
+            "id": 10,
+            "name": "Jack Bauer",
+            "age": 20,
+            "grade": "55",
+        }
+    ]
+
+export default students
--- pages/components/Export.js
+++
@@ -1,7 +0,0 @@
-import EditStudentForm from "./EditForm.js";
-import StudentForm from "./StudentFormModal.js";
-import StudentsTable from "./StudentTable.js";
-import TableComponents from "./TableComponent.js";
-import ViewStudents from "./ViewStudents.js"
-
-export { StudentsTable, StudentForm, TableComponents, EditStudentForm, ViewStudents }
